d08f8800e69abf24f05d8a53c84722b5e8d956d7
mirri66 <tsmgrace@gmail.com>
2013-10-26 09:57:01 -0700

    insert hover

--- js/insertHover.js
+++ js/insertHover.js
@@ -5,11 +5,15 @@ function insertHover(){
 			var innerhtml;
 			for (var s=0;s<dataset[x].positions[y].skills.length;s++){
 				var skillname = dataset[x].positions[y].skills[s];
+				var strip =  String(skillname.replace(/\s/g,"").toLowerCase());
 				//innerhtml += '<a href="' + skillname.replace(/ /g,'') +'.html">' + skillname +'</a><br>';
-				innerhtml += '<a class="tooltip" href="#'+ skillname.replace(/ /g,'')+'" onmouseover="tooltip.pop(this, "#'+ skillname.replace(/ /g,'')+'")">' + skillname +'</a><br>';
+				innerhtml += '<a class="tooltip" onmouseover="tooltip.pop(this, "#'+ strip +'")'
+				innerhtml +=  ' href="#'+ strip+'">' 
+				innerhtml += skillname +'</a><br>';
 				
 			}
-			node.innerHTML=innerhtml;
+			//node.innerHTML=innerhtml;
+			node.onmouseover= function(){tooltip.pop(this,"test")};
 		}
 		//document.getElementById();
 	}
