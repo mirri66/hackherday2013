c02b60ffdf40a8e1561a3bfb2ea48fc9032eff12
mirri66 <tsmgrace@gmail.com>
2013-10-26 04:35:05 -0700

    many patent attorneys

--- js/chart.js
+++ js/chart.js
@@ -1,58 +1,133 @@
 var dataset = [
-    {
+    {"positions":[{
+        "kind": "Assistant Professor",
+        "duration": 4,
+        "skills":["Independent Research", "Experiment Design"],
+      },
+      {
         "kind": "Professor",
         "duration": 12,
+        "skills":["Independent Research", "Experiment Design"],
+      },
+        ],
+    },
+    {"positions":[{
+        "kind": "Patent Attorney",
+        "duration": 3,
+        "skills":["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"],
     },
     {
         "kind": "Patent Attorney",
         "duration": 8,
+        "skills":["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"],
     },
+    ],
+  },
+  {"positions":[{
+        "kind": "Patent Attorney",
+        "duration": 3,
+        "skills":["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"],
+    },
+    {
+        "kind": "Patent Attorney",
+        "duration": 8,
+        "skills":["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"],
+    },
+    ],
+  },
+  {"positions":[{
+        "kind": "Patent Attorney",
+        "duration": 3,
+        "skills":["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"],
+    },
+    {
+        "kind": "Patent Attorney",
+        "duration": 8,
+        "skills":["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"],
+    },
+    ],
+  },
 ];
 
-
-//var dataset = d3.json("../sample.json");
+for (var x =0; x<dataset.length;x++){
 
 var svg = d3.select("#test").append("svg")
   .attr("width",1800)
-  .attr("height",900);
-var h = 100;
-var displace = 30;
-
+  .attr("height",100);
+var displace = 20;
+var xdisp=20;
 
 /* labels */
-var text = svg.selectAll("text")
+/*var text = svg.selectAll("text")
    .data(dataset)
+   //.data(jsoneg)
+   .enter()
+   .append("text") 
+   .text(function(d) {
+        return d.positions[0].kind;
+   //  return d.r_basicprofile[0];
+   })
+   .attr("text-anchor", "middle")
+   .attr("x", function(d){return d.positions[0].duration*50+xdisp;})
+   .attr("y", function(d,i){return i*50+displace;})
+   .attr("font-family", "Raleway");
+*/
+/* labels */
+//for (var x =0; x<dataset.length;x++){
+var text = svg.selectAll("text")
+   .data(dataset[x].positions)
+   //.data(jsoneg)
    .enter()
    .append("text") 
    .text(function(d) {
         return d.kind;
+   //  return d.r_basicprofile[0];
    })
    .attr("text-anchor", "middle")
-   .attr("x", function(d){return d.duration*40;})
-   .attr("y", function(d,i){return i*50+displace;})
+   .attr("x", function(d){return d.duration*50+xdisp;})
+   //.attr("y", function(d,i){return i*50+displace;})
+   .attr("y", function(){return 50+displace;})
    .attr("font-family", "Raleway");
+//};
 
 
 /* lines */   
+/*
 var lines = svg.selectAll("line")
   .data(dataset)
    .enter()
    .append("line")
    .attr("x1",0)
-   .attr("x2",function(d){return d.duration*40})
+   .attr("x2",function(d){return d.positions[0].duration*50+xdisp})
    .attr("y1",function(d,i){return i*50+displace+19})
    .attr("y2",function(d,i){return i*50+displace+19})
    .attr("stroke","#414141")
    .attr("stroke-width", 3); 
-
+*/
+//for (var x =0; x<dataset.length;x++){
+//alert(dataset[x].positions[x2].duration)
+var lines = svg.selectAll("line")
+  .data(dataset[x].positions)
+   .enter()
+   .append("line")
+   .attr("x1",0)
+   .attr("x2",function(d){return d.duration*50+xdisp})
+   //.attr("y1",function(d,i){return i*50+displace+19})
+   //.attr("y2",function(d,i){return i*50+displace+19})
+   .attr("y1",function(){return 50+displace+19})
+   .attr("y2",function(){return 50+displace+19})
+   .attr("stroke","#414141")
+   .attr("stroke-width", 3); 
+//}
 /* nodes */
+/*
 var circles = svg.selectAll("circle")
   .data(dataset)
   .enter()
   .append("circle");
 
 circles.attr("cx", function(d, i) {
-    return d.duration*40;}
+    return d.positions[0].duration*50+xdisp;}
   )
   .attr("cy", function(d,i){return i*50+displace+20})
   .attr("r", function(d) {
@@ -63,3 +138,25 @@ circles.attr("cx", function(d, i) {
   .attr("stroke-width", function(d) {
     return 5/2;
   });
+*/
+//for (var x =0; x<dataset.length;x++){
+
+var circles = svg.selectAll("circle")
+  .data(dataset[x].positions)
+  .enter()
+  .append("circle");
+
+circles.attr("cx", function(d, i) {
+    return d.duration*50+xdisp;}
+  )
+  //.attr("cy", function(d,i){return i*50+displace+20})
+  .attr("cy", function(){return 50+displace+20})
+  .attr("r", function(d) {
+   return 5;
+  })
+  .attr("fill", "yellow")
+  .attr("stroke", "orange")
+  .attr("stroke-width", function(d) {
+    return 5/2;
+  });
+};
---
+++ js/jsoneg.js
@@ -0,0 +1,49 @@
+var jsoneg = [
+/* acad */
+{"r_basicprofile":["Vice President","Computer Software"],
+"duration":1.2,
+"positions":[{"Business", "MBA", "skills":["Strategy", "Finance", "Marketing", "Business Planning"]}, 
+             {"Consulting", "Senior Analyst", "skills": ["Market analysis", "Cross function team management"]}, 
+			 {"Mid size company", "Director", "skills": ["Competitive analysis", "Corporate development"]}, 
+			 {"Small business", "Vice President", "skills": ["Strategic Partnerships", "Acquisition Integration"]}
+			 ],
+},
+{"r_basicprofile" : ["Assistant Professor","Research"],
+"duration":1.5,
+"positions":[{"Research", "Doctoral Candidate", "skills":["Computer Architecture", "Compilers", "Algorithms", "Simulations", "Graduate Research"]},
+			 {"Research", "Post Doctoral Candidate", "skills": ["Independent Research", "Experiment Design"]}, 
+			 {"Industry Research", "Senior Research Scientist", "skills":["Analytical Modeling", "Industry Collaboration", "Integration of Research idea to product"]}, 
+			 {"Academia", "Associate Professor", "skills": ["Research leader", "Teaching", "Technical consultant"]}],
+},
+/* disrupt */
+{"r_basicprofile":["Principal Architect","Computer Hardware"],
+"duration":1.5,
+"positions":[{"Computer Hardware", "Lead Architect", "skills":["Architecture Design", "Architectural Exploration", "Low power design", "Programming model"]}, 
+             {"Computer Hardware", "Chip Architect", "skills": ["Competitive analysis", "Project management", "Cross unit team schedule management"]}, 
+             {"Computer Hardware", "Senior Architect", "skills": ["Compiler analysis", "Unit Performance Analysis", "Architecture decision validation studies"]}], 
+},
+
+/* mgmt */
+{"r_basicprofile":["Vice President","Computer Software"],
+"duration":1.5,
+"positions":[{"Business", "MBA", "skills":["Strategy", "Finance", "Marketing", "Business Planning"]}, 
+             {"Consulting", "Senior Analyst", "skills": ["Market analysis", "Cross function team management"]}, 
+			 {"Mid size company", "Director", "skills": ["Competitive analysis", "Corporate development"]}, 
+			 {"Small business", "Vice President", "skills": ["Strategic Partnerships", "Acquisition Integration"]}],
+},
+/* patent */
+{"r_basicprofile":["Intellectual Property Attorney","Legal Services"],
+"duration":1.5,
+"positions":[{"Law", "JD Intellectual Property Law", "skills":[ "Patent Law", "Patent Litigation", "Patent Prosecution", "Contract Drafting"]},
+             {"Legal Services", "Associate", "skills": ["Intellectual Property Transactions and Licensing", "Litigation Drafting", "Cross function team management"]}, 
+             {"Legal Consulting", "Patent Consultant", "skills": ["Patent procurement", "Open Source Licensing", "Strategic creation procurement", "Patent enforcement"]}], 
+},
+/* sw */
+{"r_basicprofile":["CEO","Computer Software"],
+"duration":1.5,
+"positions":[{"Computer Software", "Lead Software Engineer", "skills":["Full-stack engineer", ""]}, 
+             {"Computer Software", "Chip Architect", "skills": ["Competitive analysis", "Project management", "Cross unit team schedule management"]}, 
+             {"Computer Software", "Senior Architect", "skills": ["Compiler analysis", "Unit Performance Analysis", "Architecture decision validation studies"]}], 
+
+},
+];
