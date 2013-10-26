299e7a333d03bcf0ee1d38a382492dff8d7474bb
mirri66 <tsmgrace@gmail.com>
2013-10-26 02:29:08 -0700

    d3

--- js/LI.js
+++ js/LI.js
@@ -6,14 +6,34 @@ function onLinkedInAuth() {
      IN.API.Profile("me")
      .fields("industry", "firstName","lastName","positions","headline","location","summary","specialties","email-address","interests","skills","educations")
      .result(displayProfiles);
+
+     /* hide intro text*/
+     $('#intro').hide();
+
+     /* display results */
+     displayresults();
 }
 
 function displayProfiles(profiles) {
      member = profiles.values[0];
+    
      document.getElementById("profiles").innerHTML = 
-          "<ul>"+
-          "<li id=\"" + member.id + "\">Name: " +  member.firstName + " " + member.lastName + "</li>" +
-          "<li> Industry: " + member.industry + "</li>" +
-          "</ul>";
-          console.log(profiles)
+          "<p id=\"" + member.id + "\">Welcome, " +  member.firstName + ".</p>"
+     }
+
+/* display results */
+function displayresults() {
+
+      /* hide first */
+      $('#profiles').css("display","none");
+      $('#test').css("display","none");
+      
+      /* construct chart */
+      $('#profiles').show('fast');
+      $('#test').show('slow');
+
+
+
+
+      //$('#results').fadeIn(200);
 }
---
+++ js/effects.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#slow').hide();
+    $('#slow').delay(500).fadeIn(800);
+ 
+    $('#slower').hide();
+    $('#slower').delay(2000).fadeIn(800);
+
+
+    
+});
